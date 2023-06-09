4b9dfe02cd09ce8b44414cd2ecd3a80d8ff43c36
S4adam <63969229+S4adam@users.noreply.github.com>
2023-06-09 10:48:27 +0200

    Error handling/fixes and code cleanup (#8)
    * various fixes and code improvements

--- client_utils/client_utils/base_client.py
+++ client_utils/client_utils/base_client.py
@@ -6,50 +6,52 @@ import json
 
 class BaseClient:
     headers = {}
-    
+
     def __init__(self, url: str) -> None:
         self.url = urlparse(url)
         self.api_base = url
 
     def __repr__(self) -> str:
-        return f'host: {self.host}, API base: {self.api_base}, headers: {self.headers}'
-    
+        headers = self.headers.copy()
+        headers['Authorization'] = '<hidden>'
+        return f'API base: {self.api_base}, headers: {headers}'
+
     @staticmethod
     def setDefaultHeaders(headers):
-        BaseClient.headers = headers   
-            
-    async def _get(self, url: str, headers={})-> str:
+        BaseClient.headers = headers
+
+    async def _get(self, url: str, headers={}) -> str:
         async with aiohttp.ClientSession(headers={**BaseClient.headers, **headers}) as session:
-            url=url_normalize(f'{self.api_base}/{url}')
+            url = url_normalize(f'{self.api_base}/{url}')
             async with session.get(url) as resp:
-                return await resp.json()
+                return await resp.json(content_type=None)
 
-    async def _get_stream(self, url: str, headers={})-> str:
-            async with aiohttp.ClientSession(headers={**BaseClient.headers, **headers}) as session:
-                url=url_normalize(f'{self.api_base}/{url}')
-                async with session.get(url) as response:
-                    async for line in response.content:
-                        yield line.decode('utf-8')
-
-    async def _post(self, url: str, headers = {}, data=None, config=None) -> str:
+    async def _get_stream(self, url: str, headers={}) -> str:
         async with aiohttp.ClientSession(headers={**BaseClient.headers, **headers}) as session:
-            url=url_normalize(f'{self.api_base}/{url}')
+            url = url_normalize(f'{self.api_base}/{url}')
+            async with session.get(url) as response:
+                async for line in response.content:
+                    yield line.decode('utf-8')
+
+    async def _post(self, url: str, headers={}, data=None, config=None) -> str:
+        async with aiohttp.ClientSession(headers={**BaseClient.headers, **headers}) as session:
+            url = url_normalize(f'{self.api_base}/{url}')
             if type(data) != bytes:
                 data = json.dumps(data)
             async with session.post(url, headers=headers, data=data, params=config) as resp:
-                return await resp.json()
+                return await resp.json(content_type=None)
 
-    async def _put(self, url: str, headers = {}, data=None, config=None) -> str:
+    async def _put(self, url: str, headers={}, data=None, config=None) -> str:
         async with aiohttp.ClientSession(headers={**BaseClient.headers, **headers}) as session:
-            url=url_normalize(f'{self.api_base}/{url}')
+            url = url_normalize(f'{self.api_base}/{url}')
             async with session.put(url, headers=headers, data=json.dumps(data), params=config) as resp:
                 return await resp.json()
 
-    async def _delete(self, url: str, headers = {}) -> str:
+    async def _delete(self, url: str, headers={}) -> str:
         async with aiohttp.ClientSession(headers={**BaseClient.headers, **headers}) as session:
-            url=url_normalize(f'{self.api_base}/{url}')
+            url = url_normalize(f'{self.api_base}/{url}')
             async with session.delete(url, headers=headers) as resp:
-                return await resp.json() 
+                return await resp.json()
 
     async def get_load_check(self) -> dict:
         """
@@ -57,10 +59,10 @@ class BaseClient:
 
         Returns
         -----------
-        dict: 
+        dict:
             Dict with load check informations.
         """
-        url = f'load-check'
+        url = 'load-check'
         return await self._get(url)
 
     async def get_version(self) -> dict:
@@ -72,9 +74,9 @@ class BaseClient:
         dict:
             Dict with the version of the host.
         """
-        url = f'version'
+        url = 'version'
         return await self._get(url)
-   
+
     async def get_log_stream(self) -> str:
         """
         Retrieves and yields logs from the host.
@@ -82,20 +84,20 @@ class BaseClient:
 
         Returns
         -----------
-        str: 
+        str:
             String with stream logs (UTF-8).
         """
-        url = f'log'
+        url = 'log'
         return self._get_stream(url)
-    
+
     async def get_config(self) -> dict:
         """
         Retrieves the log stream of the STH.
 
         Returns
         -----------
-        dict: 
+        dict:
             Dict with config informations.
         """
-        url = f'config'
+        url = 'config'
         return await self._get(url)
