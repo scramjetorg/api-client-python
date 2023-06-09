d994d069af087e2368d404c01414b00816cb8b3d
S4Adam <adam.mikolajewski@scramjet.org>
2023-06-09 12:40:23 +0200

    add timeout

--- client/features/environment.py
+++ client/features/environment.py
@@ -9,18 +9,27 @@ log = logging.getLogger(__name__)
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 
+def cleanup(signum=None, frame=None):
+    global process
+    os.killpg(os.getpgid(process.pid), signal.SIGTERM)
+    if signum and frame:
+        raise TimeoutError('Sth failed to start.')
+
+
 def before_all(context):
     log.info('Before all executed')
     path = '../../'
     global process
     process = subprocess.Popen(['npx', '@scramjet/sth', '--runtime-adapter=process'], cwd=path, stdin=subprocess.PIPE, stdout=subprocess.PIPE, preexec_fn=os.setpgrp)
+    signal.signal(signal.SIGALRM, cleanup)
+    signal.alarm(30)
     while True:
         output = process.stdout.readline()
         if b'Host started' in output:
+            signal.alarm(0)
             break
 
 
 def after_all(context):
     log.info('After all executed')
-    global process
-    os.killpg(os.getpgid(process.pid), signal.SIGTERM)
+    cleanup(None, None)
